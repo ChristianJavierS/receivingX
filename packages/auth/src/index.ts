77dd0a986c3f0fce4a5c579b55a5fd0705b4bc1f
ChristianJavierS <ChristianJavierS@users.noreply.github.com>
2026-08-05 12:49:43 -0500

    Initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,32 @@
+import { expo } from "@better-auth/expo";
+import { createPrismaClient } from "@receivingX/db";
+import { env } from "@receivingX/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export function createAuth() {
+  const prisma = createPrismaClient();
+
+  return betterAuth({
+    database: prismaAdapter(prisma, {
+      provider: "postgresql",
+    }),
+
+    trustedOrigins: [env.CORS_ORIGIN, "receivingX://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
